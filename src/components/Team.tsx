221d0cec7373c617274a8dd0f8d1ecf64ae23515
Dean Draper <dean@deandraper.dev>
2025-02-10 09:37:13 -0600

    Removed a scumbag dickhead from the team

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -18,12 +18,6 @@ const team = [
       "https://images.unsplash.com/photo-1494648739040-d3d4e84136a9?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    name: "Michael Landon",
-    role: "Land Surveyor, RPLS",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494648739040-d3d4e84136a9?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
     name: "Mallory Draper",
     role: "Operations Manager",
     imageUrl:
