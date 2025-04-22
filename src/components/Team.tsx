6761ad2fe77195fbdc14fa56dae597be3d93e1d0
Dean Draper <dean@deandraper.dev>
2025-04-22 11:02:56 -0500

    Mitch's Bio and Jimmie added to site

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,10 +1,6 @@
+import Mitchell from "@/components/Mitchell";
+
 const team = [
-  {
-    name: "Mitchell Lenamond",
-    role: "Founder, PE",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494648739040-d3d4e84136a9?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
   {
     name: "Jason Lenamond",
     role: "Civil Engineering Manager, PE",
@@ -26,6 +22,12 @@ const team = [
   {
     name: "Bailey Lenamond",
     role: "COO, HR",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494648739040-d3d4e84136a9?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Jimmie Kenyon",
+    role: "Senior Surveyor",
     imageUrl:
       "https://images.unsplash.com/photo-1494648739040-d3d4e84136a9?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
@@ -76,6 +78,7 @@ export default function Team() {
           project&apos;s triumph.
         </p>
       </div>
+      <Mitchell />
       <ul
         role="list"
         className="mx-auto mt-20 pb-28 grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6"
