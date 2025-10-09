e64666f069c52f5f67c887263c5739a3a78ffbe3
Dean Draper <dean@deandraper.dev>
2025-10-09 15:28:09 -0500

    Removed Jimmie Kenyon

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -25,12 +25,7 @@ const team = [
     imageUrl:
       "https://images.unsplash.com/photo-1494648739040-d3d4e84136a9?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
-  {
-    name: "Jimmie Kenyon",
-    role: "Senior Surveyor",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494648739040-d3d4e84136a9?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
+
   // More people...
 ];
 
